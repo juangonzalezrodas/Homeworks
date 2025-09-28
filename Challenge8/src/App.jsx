0df8868d1a92e090107ef08929f14eaf357275df
Juan Camilo Gonzalez <juan.gonzalez_rodas@uao.edu.co>
2025-09-27 20:34:43 -0500

    Subiendo Challenge8

---
+++ Challenge8/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react';
+import BookStackApp from './components/BookStackApp';
+
+function App() {
+  return (
+    <div>
+      <BookStackApp />
+    </div>
+  );
+}
+
+export default App;
+
+
